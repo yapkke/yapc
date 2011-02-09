40149ce299f350c81fbeb3262bdb0a75a48bc943
ykk <ykk@coin.stanford.edu>
2011-02-09 12:40:53 -0800

    Allow raw packet to be referenced directly

--- yapc/events/openflow.py
+++ yapc/events/openflow.py
@@ -59,16 +59,18 @@ class pktin(ofcomm.message):
         self.pktin = None
         ##Packet included parsed by dpkt
         self.dpkt = None
+        ##Binary packet
+        self.pkt = None
         ##Exact match for match
         self.match = None
 
         if (self.header.type == pyof.OFPT_PACKET_IN):
             self.pktin = pyof.ofp_packet_in()
-            pkt = self.pktin.unpack(msg)
+            self.pkt = self.pktin.unpack(msg)
             output.vdbg("Packet in\n"+self.pktin.show("\t"),
                         self.__class__.__name__)
             (self.match, self.dpkt) = ofutil.get_ofp_match(self.pktin.in_port, 
-                                                      pkt)
+                                                           self.pkt)
             output.vdbg("Packet has match\n"+self.match.show("\t"),
                         self.__class__.__name__)
             output.vdbg(str(`self.dpkt`),
