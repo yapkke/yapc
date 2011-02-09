56ab8f2165a334ddcc25e8efe9fd4c8cfd66b908
ykk <ykk@coin.stanford.edu>
2011-02-09 12:41:20 -0800

    Add flood pkt

--- yapc/forwarding/default.py
+++ yapc/forwarding/default.py
@@ -11,20 +11,20 @@ import yapc.events.openflow as ofevents
 import yapc.pyopenflow as pyof
 import yapc.openflowutil as ofutil
 
-class dropflow(yapc.component):
-    """Class that drop flows
+class floodpkt(yapc.component):
+    """Class that floods packet
 
     @author ykk
     @date Feb 2011
     """
-    def __init__(self, server, conn):
+    def __init__(self, server, ofconn):
         """Initialize
 
         @param server yapc core
         @param conn reference to connections
         """
-        ##Reference to connections
-        self.conn = conn
+        ##Reference to OpenFlow connections
+        self.conn = ofconn
 
         server.scheduler.registereventhandler(ofevents.pktin.name, self)
 
@@ -33,11 +33,54 @@ class dropflow(yapc.component):
 
         @param event event to handle
         """
-        if (event.sock not in self.conn.connections.db):
-            self.conn.connections.add(event.sock)
-
         if (isinstance(event, ofevents.pktin)):
-                self.dropflow(event)
+            oao = pyof.ofp_action_output()
+            oao.port = pyof.OFPP_FLOOD
+
+            po = pyof.ofp_packet_out()
+            po.header.xid = fm.header.xid
+            po.in_port = event.match.in_port
+            po.actions_len = oao.len
+            po.actions.append(oao)
+            
+            if (event.pktin.buffer_id == po.buffer_id):
+                self.conn.db[event.sock].send(po.pack()+event.pkt)
+                output.vdbg("Flood unbuffered packet with match "+\
+                                event.match.show().replace('\n',';'))
+          
+            else:
+                po.buffer_id = event.pktin.buffer_id
+                self.conn.db[event.sock].send(po.pack())
+                output.vdbg("Flood buffered packet with match "+\
+                                event.match.show().replace('\n',';'))
+
+        return True
+
+
+class dropflow(yapc.component):
+    """Class that drop flows
+
+    @author ykk
+    @date Feb 2011
+    """
+    def __init__(self, server, ofconn):
+        """Initialize
+
+        @param server yapc core
+        @param conn reference to connections
+        """
+        ##Reference to OpenFlow connections
+        self.conn = ofconn
+
+        server.scheduler.registereventhandler(ofevents.pktin.name, self)
+
+    def processevent(self, event):
+        """Event handler
+
+        @param event event to handle
+        """
+        if (isinstance(event, ofevents.pktin)):
+            self.dropflow(event)
 
         return True
 
@@ -53,7 +96,7 @@ class dropflow(yapc.component):
         fm.command = pyof.OFPFC_ADD
         fm.idle_timeout = 5
         fm.buffer_id = event.pktin.buffer_id
-        self.conn.connections.db[event.sock].send(fm.pack())
+        self.conn.db[event.sock].send(fm.pack())
 
         output.vdbg("Dropping flow with match "+\
                         event.match.show().replace('\n',';'))
