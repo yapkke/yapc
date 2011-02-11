d97d4dec854d819d139ebf9f8c41a6049c133898
KK <ykk@kk-alien>
2011-02-11 14:22:55 -0800

    Some help content for the testers

--- tests/parse_pkt_match.py
+++ tests/parse_pkt_match.py
@@ -6,9 +6,41 @@ import pcap
 import dpkt
 import sys
 
-output.set_mode("DBG")
+##Print usage guide
+def usage():
+    """Display usage
+    """
+    print "Usage "+sys.argv[0]+" [options] <file name>"
+    print "\tpcap file should contain packets for parsing"
+    print
+    print  "Options:"
+    print "-h/--help\n\tPrint this usage guide"
 
-pc = pcap.pcap(sys.argv[1])
+#Parse options and arguments
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "h",
+                               ["help"])
+except getopt.GetoptError:
+    print "Option error!"
+    usage()
+    sys.exit(2)
+
+#Parse options
+for opt,arg in opts:
+    if (opt in ("-h","--help")):
+        usage()
+        sys.exit(0)
+    else:
+        print "Unhandled option :"+opt
+        sys.exit(2)
+
+if (len(args) < 1):
+    print "Missing filename"
+    usage()
+    sys.exit(2)
+
+output.set_mode("DBG")
+pc = pcap.pcap(args[0])
 for ts,ofdata in pc:
     (ofm, dpktp) = ofutil.get_ofp_match(0, ofdata)
     output.dbg(str(`dpktp`))
