b22709f83934c9dbe32d8e482c23c099b2625d1e
ykk <ykk@coin.stanford.edu>
2011-03-10 18:44:45 -0800

    More screen commands

--- yapc/commands.py
+++ yapc/commands.py
@@ -8,6 +8,8 @@
 import os
 import yapc.output as output
 
+SCREEN = "screen"
+
 def run_cmd(cmd, classname):
     """Run command
     
@@ -33,4 +35,41 @@ def run_cmd_screen(name, cmd, classname):
     @param cmd command
     @param classname name of class calling command
     """
-    return run_cmd("screen -dm -S "+name+" -e ^Oo "+cmd, classname)
+    return run_cmd(SCREEN+" -dm -S "+name+" -e ^Oo "+cmd, classname)
+
+def kill_screen(name, classname):
+    """Kill screen
+    
+    @param name name of screen
+    @param classname name of class calling command
+    """
+    return run_cmd(SCREEN+" -d -r "+name+" -X quit", classname)
+
+def __parse_screen_list(line):
+    """Line of screen list
+    """
+    result = {}
+    s = line.split()
+    result["fullname"] = s[0]
+    ss = s[0].split(".")
+    result["pid"] = ss[0]
+    result["name"] = ss[1]
+    result["date"] = s[1][1:]
+    result["time"] = s[2]+" "+s[3]
+    result["status"] = s[4][1:-1]
+    return result
+
+def list_screen(classname):
+    """List screen
+
+    @param classname name of class calling command
+    """
+    (ret, out) = run_cmd(SCREEN+" -ls", classname)
+    if (len(out) == 2):
+        return None
+    else:
+        screenlist = []
+        for s in out[1:-2]:
+            screenlist.append(__parse_screen_list(s))
+        return screenlist
+
