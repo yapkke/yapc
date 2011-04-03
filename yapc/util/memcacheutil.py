7fc81dc2629aec43f074e795f1017a2d284e38c9
ykk <ykk@openflow2.stanford.edu>
2011-04-03 14:20:42 -0700

    Local memcache

--- yapc/util/memcacheutil.py
+++ yapc/util/memcacheutil.py
@@ -6,6 +6,7 @@
 # @date Feb 2011
 #
 import memcache
+import time
 import yapc.output as output
 
 ##Global memcache client
@@ -17,8 +18,11 @@ global memcache_servers
 memcache_servers = ['127.0.0.1:11211']
 
 ##On/Off switch
-global memcache_on
-memcache_on = True
+global memcache_mode
+MEMCACHE_MODE = {"ON":0,
+                 "OFF" = 1,
+                 "LOCAL" = 2}
+memcache_mode = MEMCACHE_MODE["ON"]
 
 ##Memcache debug
 DEBUG = 1
@@ -31,14 +35,17 @@ def get_client():
     """
     global memcache_client
     global memcache_servers
-    global memcache_on
-    if (not memcache_on):
+    global memcache_mode
+    if (memcache_mode == MEMCACHE_MODE["OFF"]):
         return 
-
+ 
     if (memcache_client== None):
-        memcache_client = memcache.Client(memcache_servers,
-                                          DEBUG)
-        memcache_client.flush_all()
+        if (memcache_mode == MEMCACHE_MODE["ON"]):
+            memcache_client = memcache.Client(memcache_servers,
+                                              DEBUG)
+            memcache_client.flush_all()
+        elif (memcache_mode == MEMCACHE_MODE["LOCAL"]):
+            memcache_client = local_memcache()
     return memcache_client
 
 def set(name, value, timeout=0):
@@ -84,3 +91,40 @@ def socket_str(sock):
     """Get string for socket
     """
     return str(`sock`).replace(" ","").strip()
+
+class local_memcache:
+    """Class that stores key value like memcache locally
+
+    @author ykk
+    @date Apr 2011
+    """
+    def __init__(self):
+        """Initialize
+        """
+        #Dictionary
+        self.dict = {}
+
+    def get(self, name):
+        """Get value with key
+        """
+        try:
+            (value, timeout) = self.dict[name]
+            if ((timeout == 0) or (timeout > time.time())):
+                return value
+            else:
+                self.delete(name)
+        except KeyError:
+            return None
+
+    def set(self, name, value, timeout):
+        """Set value with key
+        """
+        if (timeout == 0):
+            self.dict[name] = (value, timeout)
+        else:
+            self.dict[name] = (value, time.time()+timeout)
+
+    def delete(self, name):
+        """Delete key
+        """
+        del self.dict[name]
