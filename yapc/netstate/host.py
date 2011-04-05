5a32eb0a4df0d095cdf6f948b94e68e6bf52ccae
ykk <ykk@coin.stanford.edu>
2011-04-04 21:50:28 -0700

    Allow host DNS to be access via memcache

--- yapc/netstate/host.py
+++ yapc/netstate/host.py
@@ -28,12 +28,12 @@ class host_dns(yapc.component):
         """
         server.register_event_handler(ofevents.pktin.name, self)
 
-    def get_name_key(domain_name):
+    def get_name_key(host, domain_name):
         """Get key for domain name
 
         @param domain_name domain name
         """
-        return host_dns.HOST_DNS_DOMAIN_PREFIX+domain_name
+        return host_dns.HOST_DNS_DOMAIN_PREFIX+host+"_"+domain_name
     get_name_key = yapc.static_callable(get_name_key)
 
     def get_addr_key(ipaddr):
@@ -41,22 +41,37 @@ class host_dns(yapc.component):
 
         @param ipaddr IP address in value
         """
-        return host_dns.HOST_DNS_IP_PREFIX+domain_name
-    get_name_key = yapc.static_callable(get_name_key)
+        return host_dns.HOST_DNS_IP_PREFIX+ipaddr
+    get_addr_key = yapc.static_callable(get_addr_key)
 
     def processevent(self, event):
         """Handle event to sniff domain name-IP address binding
         """
         if (isinstance(event, ofevents.pktin)):
             if (event.match.tp_src == 53):
-                dnsreply = dpkt.dns.DNS(event.dpkt["data"]["data"]["data"])               
+                dnsreply = dpkt.dns.DNS(event.dpkt["data"]["data"]["data"])
+                nameaddr = {}
+
+                for q in dnsreply["qd"]:
+                    nameaddr[q["name"]] = []
+
                 for rtype in ["an","ar"]:
                     for rr in dnsreply[rtype]:
+                        mc.set(host_dns.get_addr_key(socket.inet_ntoa(rr["rdata"])),
+                               rr["name"], rr["ttl"])
+
+                        if (rtype == "an"):
+                            nameaddr[rr["name"]].append(rr["rdata"])
+                            
                         output.dbg(rtype+" record: "+\
-                                       rr["name"]+" bindded to "+socket.inet_ntoa(rr["rdata"])+\
+                                       rr["name"]+" binded to "+socket.inet_ntoa(rr["rdata"])+\
                                        " with TTL of "+str(rr["ttl"])+ "s for host "+\
                                        pu.array2hex_str(event.match.dl_dst),
                                    self.__class__.__name__)
+
+                for name,val in nameaddr.items():
+                    output.dbg(name+"=>"+str(len(val))+" IP addresses",
+                               self.__class__.__name__)
 
         return True
         
