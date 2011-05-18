c3085d28b947c241218fbb3de5647a8a8c6398c0
ykk <ykk@openflow2.stanford.edu>
2011-05-17 23:20:17 -0700

    Basic SQLite done with test case

--- tests/test_sqlite.py
+++ tests/test_sqlite.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python
-
 import yapc.log.output as output
 import yapc.log.sqlite as sqlite
+import os
 
 output.set_mode("VDBG")
 
-db = sqlite.Database("testing.sqlite")
+filename="testing.sqlite"
+try:
+    os.remove(filename)
+except OSError:
+    pass
+
+db = sqlite.Database(filename)
 db.add_table(sqlite.Table("Test1", ["col1", "col2"]))
 db.add_table(sqlite.Table("Test2", ["col12", "col22"]))
 db.create_tables()
+db.tables["Test1"].add_row((1.0,2.0))
+db.tables["Test1"].add_row((2.0,2.0))
+db.tables["Test1"].add_row((3.0,2.0))
+db.tables["Test2"].add_row(("3.0",20))
+db.flush()
 
+for row in db.tables["Test1"].select():
+    print row
+for row in db.tables["Test2"].select():
+    print row
 
-
+db.close()
--- yapc/log/sqlite.py
+++ yapc/log/sqlite.py
@@ -17,13 +17,16 @@ class Database:
         self.filename = filename
         ##Connection
         self.connection = sqlite3.connect(self.filename)
+        output.dbg("Opening database "+self.filename)
         self.connection.row_factory = sqlite3.Row
         ##List of tables
         self.tables = {}
 
-    def __del__(self):
+    def close(self):
         """Finalize
         """
+        output.dbg("Closing database")
+        self.connection.commit()
         self.connection.close()
 
     def add_table(self, table):
@@ -36,12 +39,30 @@ class Database:
         """
         for tname,tab in self.tables.items():
             tab.create(self)
+        self.connection.commit()
 
     def execute(self, stmt):
         """Execute statement
         """
         output.vdbg(stmt, self.__class__.__name__)
-        self.connection.execute(stmt)
+        return self.connection.execute(stmt)
+
+    def executemany(self, stmt, data):
+        """Execute many statements
+        """
+        return self.connection.executemany(stmt, data)
+
+    def commit(self):
+        """Commit commands
+        """
+        self.connection.commit()
+
+    def flush(self):
+        """Flush cache of all tables
+        """
+        for tname,tab in self.tables.items():
+            tab.flush_cache()
+        self.connection.commit()
 
 class Table:
     """SQLite table
@@ -49,11 +70,12 @@ class Table:
     @author ykk
     @date May 2011
     """
-    def __init__(self, name, columns=None):
+    def __init__(self, name, columns=None, cs=100):
         """Initialize
 
         @param name name of table
         @param columns list of column names
+        @param cs max cache size to maintain
         """
         ##Name of table
         self.name = name
@@ -61,7 +83,13 @@ class Table:
         self.columns = columns
         if (columns == None):
             self.columns = []
-    
+        ##Cache of data to add
+        self.data_cache = []
+        ##Cache max
+        self.cache_size = cs
+        ##Reference to db
+        self.db = None
+
     def add_column(self, name):
         """Add column with name
         """
@@ -85,4 +113,56 @@ class Table:
 
         @param db reference to database
         """
+        self.db = db
         db.execute(self.create_stmt())
+
+    def add_row(self, row):
+        """Add data to the table
+
+        @param row  is a tuple of data
+        """
+        if (len(row) == len(self.columns)):
+            output.vdbg("Add row :"+str(row))
+            self.data_cache.append(row)
+            if (len(self.data_cache) >= self.cache_size):
+                self.flush_cache()
+        else:
+            output.warn("Adding row "+str(row)+" with "+len(row)+" items"+\
+                            " when there is "+len(self.columns)+" columns",
+                        self.__class__.__name__)
+
+    def flush_cache(self):
+        """Flush data in cache in database
+        """
+        if (self.db == None):
+            output.warn("No database to flush data into",
+                        self.__class__.__name__)
+            return
+
+        stmt = "?,"*len(self.columns)
+        self.db.executemany("INSERT INTO "+self.name+\
+                                " VALUES ("+stmt[:-1]+")",
+                            self.data_cache)
+        self.db.commit()
+        self.data_cache = []
+
+    def select_stmt(self, selection="*", where=None):
+        """Create the following:
+           SELECT <select> FROM <table> WHERE <where>
+
+        @param selection selection to return
+        @param where where condition
+        """
+        r = "SELECT "+selection+" FROM "+self.name
+        if (where != None):
+            r += "WHERE "+where
+        return r
+
+    def select(self, selection="*", where=None):
+        """Execute the following:
+           SELECT <select> FROM <table> WHERE <where>
+
+        @param selection selection to return
+        @param where where condition
+        """
+        return self.db.execute(self.select_stmt(selection, where))
