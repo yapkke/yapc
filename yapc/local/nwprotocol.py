56ab55a2a4959460f2676a36f9d6b46ff94fc193
ykk <ykk@coin.stanford.edu>
2011-05-30 17:07:10 -0700

    Add memcache setup to dhcp

--- yapc/local/nwprotocol.py
+++ yapc/local/nwprotocol.py
@@ -8,6 +8,7 @@
 import yapc.interface as yapc
 import yapc.log.output as output
 import yapc.events.openflow as ofevents
+import yapc.util.memcacheutil as mcutil
 import dpkt
 
 class dhcp(yapc.component):
@@ -18,17 +19,30 @@ class dhcp(yapc.component):
     @author ykk
     @date May 2011
     """
+    #Key prefix for datapath_id and port
+    DP_PORT_KEY_PREFIX = "dhcp_dp_port_"
     def __init__(self, server, ofconn):
         """Initialize
 
         @param server yapc core server/scheduler
         @param ofconn OpenFlow connections
         """
+        #Start memcache
+        mc.get_client()
         ##OpenFlow connections
         self.ofconnections = ofconn
         
         server.register_event_handler(ofevents.pktin.name,
                                       self)
+
+    def get_key(dpid, port):
+        """Get key for IP address and gateway IP
+
+        @param dpid datapath id of switch
+        @param port port number of interface
+        """
+        return dhcp.DP_PORT_KEY_PREFIX+"%x_" % dpid+str(port).strip()
+    get_key = yapc.static_callable(get_key)
 
     def processevent(self, event):
         """Process OpenFlow messages
