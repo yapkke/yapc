bce1caa2cc87c44e2420d5760784d1f861a8030c
ykk <ykk@coin.stanford.edu>
2011-07-11 12:31:10 -0700

    The proper flow splitter

--- bin/bitw-flowsplitter.py
+++ bin/bitw-flowsplitter.py
@@ -4,8 +4,10 @@ import yapc.log.output as output
 import yapc.interface as yapc
 import yapc.local.netintf as neti
 import yapc.local.packetprocessor as pp
+import yapc.util.parse as pu
 import sys
 import getopt
+import dpkt
 
 class flowsplitter(pp.bitw):
     """A base class for Bump-in-the-wire
@@ -13,35 +15,50 @@ class flowsplitter(pp.bitw):
     @author ykk
     @date July 2011
     """
-    def __init__(self, server, intf, extintf):
+    def __init__(self, server, intf, extintf, ip):
         """Initialize
 
         @param server reference to yapc core
         @param intf name of interface to attach bump in the wire
         @param extintf name of external interface to attach bump in the wire
+        @param ip list of destination IP to use (first being the primary one)
         """
         pp.bitw.__init__(self, server, intf, extintf)
-        output.dbg("Setting up flow splitter from "+extintf+" to "+intf,
-                   self.__class__.__name__)
+        self.extintf = extintf
+        self.ip = ip
+        self.__index = 0
+        output.info("Setting up flow splitter from "+extintf+" to "+intf+\
+                        " for splitting flow among "+str(self.ip),
+                    self.__class__.__name__)
 
-    def processpacket(self, rawmsg):
+
+    def process(self, packet):
         """Process packet
- 
-        Simply send on the other side
         """
-        pkt = rawmsg.message
-        if (rawmsg.sock == self.intf1.sock):
-            self.intf2.sock.send(pkt)
-        if (rawmsg.sock == self.intf2.sock):
-            self.intf1.sock.send(pkt)
+        pkt = dpkt.ethernet.Ethernet(packet)
+        if ((pkt["type"] == dpkt.ethernet.ETH_TYPE_IP) and
+            (pkt["data"]["dst"] == pu.ip_string2binary(self.ip[0])) and
+            (pkt["data"]["p"] == 6)):
 
-        return True
+            pkt["data"]["dst"] = self.ip[self.__index]
+            pkt["data"]["sum"] = 0
+            pkt["data"]["data"]["sum"] = 0
+
+            self.__index += 1
+            if (self.__index >= len(self.ip)):
+                self.__index = 0
+
+            return pkt.pack()
+        else:
+            return packet
 
 class bitw_flowsplitter(yapc.daemon):
     def __init__(self):
         yapc.daemon.__init__(self)
         ##External interface
         self.extintf = "eth1"
+        ##IP to send to
+        self.ip = []
         ##Internal interface
         self.__intf = None
 
@@ -60,8 +77,8 @@ class bitw_flowsplitter(yapc.daemon):
 
         self.configure_if(intfmgr)
         fs = flowsplitter(server, self.extintf,
-                          intfmgr.veth[0].names[0])
-
+                          intfmgr.veth[0].names[0], 
+                          self.ip)
         server.run()
         sys.exit(0)
 
@@ -69,8 +86,9 @@ class bitw_flowsplitter(yapc.daemon):
 def usage():
     """Display usage
     """
-    print "Usage "+sys.argv[0]+""
+    print "Usage "+sys.argv[0]+" ext_interface dst_ip_1 dst_ip_2 ..."
     print "\tBump-in-the-wire (TCP flow splitter)"
+    print "\tSplit TCP flow to destination in a round-robin manner"
     print  "Options:"
     print "-h/--help\n\tPrint this usage guide"
     print "-v/--verbose\n\tVerbose output"
@@ -104,5 +122,8 @@ for opt,arg in opts:
     else:
         print "Unhandled option :"+opt
         sys.exit(2)
+
+bfs.extintf = args[0]
+bfs.ip = args[1:]
 
 bfs.start()
