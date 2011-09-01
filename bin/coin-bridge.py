56ce6c1556fdda6e692dd30c54f92e02a53556f1
ykk <ykk@coin.stanford.edu>
2011-09-01 16:13:58 -0700

    Basic bridge skeleton

--- bin/coin-bridge.py
+++ bin/coin-bridge.py
@@ -5,8 +5,13 @@
 #
 import yapc.core as core
 import yapc.interface as yapc
+import yapc.coin.core as coin
+import yapc.coin.bridge as coinbr
+import yapc.coin.ovs as coinovs
 import yapc.comm.openflow as ofcomm
+import yapc.comm.json as jsoncomm
 import yapc.events.openflow as ofevents
+import yapc.netstate.switches as switches
 import yapc.log.output as output
 import sys
 import getopt
@@ -16,13 +21,36 @@ class coin_bridge(yapc.daemon):
         yapc.daemon.__init__(self)
         ##Interfaces to add
         self.interfaces = []
+        ##Force JSON connection or not
+        self.forcejson = False
+        ##Socket to talk to
+        self.sock = coin.SOCK_NAME
 
     def run(self):
         server = core.core()
         ofconn = ofcomm.ofserver(server)
+        jsonconn = jsoncomm.jsonserver(server, file=self.sock, 
+                                       forcebind=self.forcejson)
 
         #OpenFlow Parser
         ofparse = ofevents.parser(server)
+        #COIN main sever, maintaining connection
+        coinserver = coinbr.bridge(server, ofconn.connections, 
+                                   jsonconn.connections)
+
+        #Network status
+        sw = switches.dp_features(server)
+
+        #OVS fabric manager
+        ovs = coinovs.switch(server, jsonconn.connections)
+        coinserver.switch = ovs
+
+        #Add interfaces
+        coinserver.setup(self.interfaces)
+
+        server.order_handler(ofevents.features_reply.name,
+                             sw, coinserver)
+        server.order_cleanup(ofconn, ovs)
 
         server.run()
         sys.exit(0)
@@ -39,15 +67,18 @@ def usage():
     print "--vs\n\tVerbose output for selected component"
     print "--very-verbose\n\tVery verbose output"
     print "-d/--daemon\n\tRun as daemon"
+    print "-f/--force-json\n\tForced binding for JSON UNIX socket"
+    print "-s/--sock\n\tSocket to communicate to (default: "+coin.SOCK_NAME+")"
     print "-i/--interface\n\tSpecify interface to add"
 
 coinb = coin_bridge()
 
 #Parse options and arguments
 try:
-    opts, args = getopt.getopt(sys.argv[1:], "hvdi:",
+    opts, args = getopt.getopt(sys.argv[1:], "hvdi:s:f",
                                ["help","verbose","daemon", 
-                                "very-verbose","vs=", "interface="])
+                                "very-verbose","vs=", "interface=",
+                                "sock=","force-json"])
 except getopt.GetoptError:
     print "Option error!"
     usage()
@@ -70,6 +101,10 @@ for opt,arg in opts:
         coinb.daemon=True
     elif (opt in ("-i","--interface")):
         coinb.interfaces.append(arg)
+    elif (opt in ("-f","--force-json")):
+        coinb.forcejson=True
+    elif (opt in ("-s","--sock")):
+        coinb.sock = arg
     else:
         print "Unhandled option :"+opt
         sys.exit(2)
--- yapc/coin/bridge.py
+++ yapc/coin/bridge.py
@@ -6,6 +6,11 @@
 # @date Sept 2011
 #
 import yapc.coin.core as core
+import yapc.log.output as output
+import yapc.util.memcacheutil as mc
+import yapc.events.openflow as ofevents
+import yapc.comm.json as jsoncomm
+import simplejson
 
 class bridge(core.coin_server):
     """Class to handle connections and configuration for COIN in Bridge mode
@@ -13,6 +18,10 @@ class bridge(core.coin_server):
     @author ykk
     @date Sept 2011
     """
+    ##Key for inner port
+    SW_INNER_PORT = "COIN_SW_INNER_PORT"
+    ##Key for switch feature
+    SW_FEATURE = "COIN_SW_FEATURE"
     def __init__(self, server, ofconn, jsonconn):
         """Initialize
 
@@ -23,4 +32,107 @@ class bridge(core.coin_server):
         core.coin_server.__init__(self, server, ofconn, jsonconn, False)
         ##Mode
         self.config["mode"] = "Multi-Homed (Bridged)"
+        ##Reference to local interface
+        self.loif = None
+        
+        mc.get_client()
 
+        server.register_event_handler(ofevents.error.name,
+                                      self)
+        server.register_event_handler(ofevents.features_reply.name,
+                                      self)
+        server.register_event_handler(ofevents.port_status.name,
+                                      self)
+        server.register_event_handler(jsoncomm.message.name,
+                                      self)
+
+    def setup(self, interfaces):
+        """Add interfaces
+        
+        @param interfaces list of interfaces
+        """
+        #Set up interfaces
+        self.loif = self.add_loif("local")
+        self.add_interfaces(interfaces)
+
+        #ifup each interface
+        for i in range(0, len(interfaces)):
+            self.ifmgr.up(interfaces[i])
+
+    def update_sw_feature(self):
+        """Update switch feature in memcache
+        """
+        sf = self.switch.get_sw_feature()
+        if (sf == None):
+            output.warn("No switch features!!!",
+                        self.__class__.__name__)
+        else:
+            output.dbg("Set switch feature as "+sf.show(),
+                       self.__class__.__name__)
+            mc.set(bridge.SW_FEATURE, sf)
+        
+        iport = self.switch.if_name2dpid_port_mac(self.loif.switch_intf)[1]
+        if (iport == None):
+            output.warn("No inner port!!!",
+                        self.__class__.__name__)
+        else:
+            output.dbg("Set inner port as "+str(iport),
+                       self.__class__.__name__)
+            mc.set(bridge.SW_INNER_PORT, iport)
+
+    def processevent(self, event):
+        """Process OpenFlow and JSON messages
+
+        @param event event to handle
+        @return True
+        """
+        if (isinstance(event, ofevents.features_reply) or
+            isinstance(event, ofevents.port_status)):
+            self.update_sw_feature()
+        elif isinstance(event, ofevents.error):
+            #OpenFlow error
+            output.warn("Error of type "+str(event.error.type)+\
+                            " code "+str(event.error.code),
+                        self.__class__.__name__)
+        elif isinstance(event, jsoncomm.message):
+            #JSON messages
+            self.__processjson(event)
+
+        return True
+
+    def __processjson(self, event):
+        """Process basic JSON messages
+        
+        @param event yapc.jsoncomm.message event
+        """        
+        if (event.message["type"] == "coin" and
+            event.message["subtype"] == "global"):
+            reply = self._processglobal(event)
+            if (reply != None):
+                self.jsonconnections.db[event.sock].send(reply)
+        elif (event.message["type"] == "coin" and
+            event.message["subtype"] == "loif"):
+            reply = self.__processloif(event)
+            if (reply != None):
+                self.jsonconnections.db[event.sock].send(reply)
+        else:
+            output.dbg("Receive JSON message "+simplejson.dumps(event.message),
+                       self.__class__.__name__)
+
+    def __processloif(self, event):
+        """Process local interfaces related JSON messages
+        
+        @param event yapc.jsoncomm.message event
+        """
+        reply = {}
+        reply["type"] = "coin"
+        reply["subtype"] = "loif"
+
+        if (event.message["command"] == "create_lo_intf"):
+            self.add_loif(event.message["name"])
+        else:
+            output.dbg("Receive message "+str(event.message),
+                       self.__class__.__name__)
+            return None
+
+        return reply
